a94139023e8628e861ccb3b48c0f95830a61efee
cybert22 <40509580+cybert22@users.noreply.github.com>
2020-03-25 10:46:34 -0500

    Add files via upload

---
+++ vista/plugins/index.php
@@ -0,0 +1,9 @@
+<?php
+
+   require_once "modelo/enlaces.php";
+   require_once "controlador/controlador.php";
+   require_once "modelo/crud.php";
+    $paginas = new MvcControlador();
+    $paginas->pagina();
+
+?>
