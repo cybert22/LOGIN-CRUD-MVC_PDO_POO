3c72a962d3fdfa13f53dbda23eda48beaf7b1ccc
cybert22 <40509580+cybert22@users.noreply.github.com>
2020-03-25 11:54:48 -0500

    Delete index.php

--- vista/plugins/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-   require_once "modelo/enlaces.php";
-   require_once "controlador/controlador.php";
-   require_once "modelo/crud.php";
-    $paginas = new MvcControlador();
-    $paginas->pagina();
-
-?>
